fd19b04df92628f9b71faeaa55e03c30c87acff5
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-07-28 10:11:42 +1200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace DSD01CalculaterForStudents.Pages
+    [BindProperties]
+{
+    public class IndexModel : PageModel
+    {
+    [Display (Name = "FirstNumber")]
+
+    [Display (Name = "SecondNumber")]
+
+
+    [Display (Name = "Result") ]
+        
+        public double NumberA { get; set; }
+
+        public double NumberB { get; set; }
+
+        public double Result { get; set; }
+        public void OnGet()
+        {
+
+        }
+    public void OnPost()
+        {
+
+        }
+
+    }
+}
